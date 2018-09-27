83ad30d723ecc980b696a84f5e33cac4c01a03ed
akhil3060 <Akhil3060@msitprogram.net>
2018-09-27 11:57:20 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1-assignment/m4/4.2 Evaluate Postfix/Solution.java
+++ ADS-1-assignment/m4/4.2 Evaluate Postfix/Solution.java
@@ -1,10 +1,29 @@
 import java.util.Scanner;
-import java.util.Stack;
+// import java.util.Stack;
 import java.util.Arrays;
 
 
 
 class Solution {
+    static private int top;
+    static private int MAXSIZE;
+    static private int[] stackArr;
+    Solution() {
+        top = -1;
+        MAXSIZE = 100;
+        stackArr = new int[MAXSIZE];
+    }
+
+    static void stPush(int val) {
+        System.out.println("top = " + top);
+        // top-=1;
+        stackArr[++top] = val;
+    }
+    static int stPop() {
+        return stackArr[top--];
+    }
+
+
     static boolean isAnumber(String num) {
         try {
             int a = Integer.parseInt(num);
@@ -13,37 +32,35 @@ class Solution {
         }
         return true;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] line = sc.nextLine().split(" ");
         // System.out.println(Arrays.toString(line));
-        Stack<Integer> st = new Stack<>();
+        Solution st = new Solution();
         for (int i = 0; i < line.length; i++) {
             if (isAnumber(line[i])) {
-                st.push(Integer.parseInt(line[i]));
+                st.stPush(Integer.parseInt(line[i]));
             } else {
-                int a = st.pop();
-                int b = st.pop();
+                int a = st.stPop();
+                int b = st.stPop();
                 int ans = 0;
                 switch (line[i]) {
                 case "*":
                     ans = a * b;
-                    st.push(ans);
+                    st.stPush(ans);
                     break;
-
                 case "+":
                     ans = a + b;
-                    st.push(ans);
+                    st.stPush(ans);
                     break;
-
                 case "-":
                     ans = a - b;
-                    st.push(ans);
+                    st.stPush(ans);
                     break;
-
                 case "/":
                     ans = a / b;
-                    st.push(ans);
+                    st.stPush(ans);
                     break;
 
 
@@ -52,7 +69,7 @@ class Solution {
 
         }
         // while (!st.isEmpty()) {
-        int myAns=st.pop();
+        int myAns = st.stPop();
         System.err.println(myAns);
         // }
     }
