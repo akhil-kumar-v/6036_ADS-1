ebf55022fd8c0e16762db06e48c2d2725732984f
akhil3060 <Akhil3060@msitprogram.net>
2018-10-08 09:39:04 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

---
+++ ADS-1-assignment/m8/8.1 Team Ranking With Insertion/Team.java
@@ -0,0 +1,37 @@
+class Team {
+	private CricketTeam[] teams;
+	private int size;
+	Team() {
+		teams = new CricketTeam[10];
+		size = 0;
+	}
+	public void addCricketTeam(CricketTeam t) {
+		teams[size++] = t;
+	}
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < size; i++) {
+			s += teams[i].name+",";
+		}
+		return s.substring(0, s.length() - 1);
+	}
+	public void exch(CricketTeam[] teams, int i, int j) {
+		CricketTeam ct = teams[j];
+		teams[j] = teams[i];
+		teams[i] = ct;
+	}
+	public void selectionSort() {
+		for (int i = 0; i < size - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < size; j++) {
+				if (less(teams, j, min)) {
+					min = j;
+				}
+			}
+			exch(teams, i, min);
+        }
+	}
+	boolean less(CricketTeam[] arr, int i, int j) {
+		return  arr[i].compareTo(arr[j]) > 0;
+	}
+}
