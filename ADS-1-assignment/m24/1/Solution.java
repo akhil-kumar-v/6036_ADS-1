34caaf58ad958eca1c86b62c64776f364d27a870
akhil3060 <Akhil3060@msitprogram.net>
2018-10-27 12:07:14 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignment/m24/1/Solution.java
+++ ADS-1-assignment/m24/1/Solution.java
@@ -3,17 +3,48 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
-
+/**
+ * Class for student.
+ */
 class Student {
+    /**
+     * Student Name.
+     */
     String name;
+    /**
+     * Student Roll.
+     */
     int roll;
+    /**
+     * Student marks.
+     */
     double marks;
-
+    /**
+     * Constructs the object.
+     *
+     * @param      na    { parameter_description }
+     * @param      ro    { parameter_description }
+     * @param      ma    { parameter_description }
+     */
     Student(String na, int ro, double ma) {
+        /**
+         * name.
+         */
         this.name = na;
+        /**
+         * Roll no.
+         */
         this.roll = ro;
+        /**
+         * Marks.
+         */
         this.marks = ma;
     }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return this.name + " " + this.roll + " " +  this.marks;
     }
@@ -21,38 +52,40 @@ class Student {
 }
 
 
-
+/**
+ * Class for solution.
+ */
 class Solution {
-    static ArrayList<Student> stlist;
+    /**
+     * Constructs the object.
+     */
     Solution() {
-        stlist = new ArrayList<Student>();
     }
+    /**
+     * Main.
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        // StudentArr sa = new StudentArr();
-        // sa.arr1
         int[] arr = new int[n];
-        // MyDS ds = new MyDS();
         Map<Integer, Student> map = new HashMap<Integer, Student>();
-
         sc.nextLine();
-        int i = 0;
+        // int i = 0;
         while (n > 0) {
             n--;
             String[] line = sc.nextLine().split(",");
             Student st = new Student(line[1], Integer.parseInt(line[0]), Double.parseDouble(line[2]));
-            // ds.add(Integer.parseInt(line[0]));
-            // System.out.println(st);
-            arr[i] = Integer.parseInt(line[0]);
-            i++;
+            // arr[i] = Integer.parseInt(line[0]);
+            // i++;
             map.put(Integer.parseInt(line[0]), st);
         }
         MergeSort ob = new MergeSort();
-
         ob.sort(arr, 0, arr.length - 1);
         int q = sc.nextInt();
         sc.nextLine();
+
         while (q > 0) {
             q--;
             String[] line = sc.nextLine().split(" ");
